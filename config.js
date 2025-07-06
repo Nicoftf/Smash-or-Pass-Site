f378b15d49eeabdc6a715ca55b1e01ce01f3886c
Nicoftf <Hoerbrand-2002@web.de>
2025-07-06 17:41:20 +0200

    Fix

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+            apiKey: "AIzaSyD_QMcoA-WNS9DHzx-dAzJAiHHEvsKbBU0",
+            authDomain: "candecate-smash-or-pass.firebaseapp.com",
+            projectId: "candecate-smash-or-pass",
+            storageBucket: "candecate-smash-or-pass.firebasestorage.app",
+            messagingSenderId: "264782387291",
+            appId: "1:264782387291:web:412ccb8f32672cb9c0c77e",
+            measurementId: "G-P5Z7889WSS"
+        };
